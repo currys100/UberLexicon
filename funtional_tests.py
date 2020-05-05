3dba832ac496b0273de67335a3e40fa54e277655
currys100 <sarah.k.curry@gmail.com>
2020-05-05 16:23:09 -0700

    successfully hosts localhost

--- funtional_tests.py
+++ funtional_tests.py
@@ -14,18 +14,18 @@ import geckodriver_autoinstaller
 
 class TestLaunchingDatabase(unittest.TestCase):
 
-    def test_django_selenium_geckodriver_installed(self):
-        geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
-        # and if it doesn't exist, download it automatically,
-        # then add geckodriver to path
-
-        driver = webdriver.Firefox()
-        driver.get("http://www.python.org")
-        assert "Python" in driver.title
+    # def test_django_selenium_geckodriver_installed(self):
+    #     geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
+    #     # and if it doesn't exist, download it automatically,
+    #     # then add geckodriver to path
+    #
+    #     driver = webdriver.Firefox()
+    #     driver.get("http://www.python.org")
+    #     assert "Python" in driver.title
 
     def test_open_browser(self):
         browser = webdriver.Firefox()
-        browser.get('https://www.djangoproject.com/')
+        browser.get('http://localhost:8000')
         assert 'Django' in browser.title
 
 
