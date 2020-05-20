ef5b810bf31accedf50ffca862f955363d867313
currys100 <sarah.k.curry@gmail.com>
2020-05-19 17:04:59 -0700

    now using bootstrap stylesheets to pass basic functional test for inputbox placement on page.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,7 +16,7 @@ To Do:
 
 """
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -28,7 +28,7 @@ RUN_ONE = True
 
 
 # noinspection SpellCheckingInspection
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
     Functional tests to determine if database and website are launched according to plan.
     """
@@ -131,7 +131,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
 
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2,
-                               512,
+                               350,
                                delta=10)
 
         # She starts a new list and sees the input is nicely
@@ -142,6 +142,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            350,
             delta=10)
 
